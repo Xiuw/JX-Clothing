efa71129865fe791f2d2b66d5343d239c0aa7da2
xiuw <xiuneh@Xius-Air.home>
2019-08-02 23:06:18 -0400

    components for collection and its items

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 import React from 'react';
 import Homepage from './components/pages/homepage/Homepage';
+import ShopContentPage from './components/pages/shop/ShopContentPage';
 import {Route , Switch} from 'react-router-dom';
 import './App.css';
 
-const HatsPage = () =>{
-  return(
-    <p>Hat Page</p>
-  )
-}
+// const HatsPage = () =>{
+//   return(
+//     <p>Hat Page</p>
+//   )
+// }
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={Homepage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopContentPage} />
       </Switch>
     </div>
   );
